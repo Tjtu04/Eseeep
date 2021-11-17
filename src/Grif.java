f45252d6297ecd6ff807a11553f46d20f2da7f70
TIlekzhan <92780361+Tjtu04@users.noreply.github.com>
2021-11-17 22:17:26 +0600

    .

---
+++ src/Grif.java
@@ -0,0 +1,14 @@
+public class Grif {
+    private String name;
+    public int count;
+    public Grif(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+    public void nastroika() {
+        System.out.println( name + " nastraivaetsya");
+    }
+    public void ZamenaStruni() {
+        System.out.println( count + " struna zamenayetsa");
+    }
+}

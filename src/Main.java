1400d1f93fd43c755a2bc3e623e2a7a3ffce28a7
TIlekzhan <92780361+Tjtu04@users.noreply.github.com>
2021-11-17 21:53:12 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Struna guitar = new Struna("Classic guitar" ,6);
+        guitar.play();
+        guitar.nastroika();
+        guitar.ZamenaStruni();
+    }
+}
